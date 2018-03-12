39727d391dc4b4b9b453c434b950424d842d8f78
CameronCooke123 <camo246810@yahoo.com>
2018-03-12 17:25:16 -0500

    Add files via upload

---
+++ Ch07_3.py
@@ -0,0 +1,50 @@
+"""
+
+	a program that reads names from two txt files,
+	then asks the user to input a name,
+	then tells them if the name that was input is on either of the lists
+
+"""
+
+
+def main():
+	boy_names, girl_names = read_files()
+	user_input = input("Enter a name: ")
+	find_name_in_file(user_input, boy_names, girl_names)
+
+
+def read_files():
+	boy_file = open("BoyNames.txt", 'r')
+	girl_file = open("GirlNames.txt", 'r')
+
+	boys_names = []
+
+	line = boy_file.readline()
+	line = line.strip("\n")
+	while line != "":
+		boys_names.append(line)
+		line = boy_file.readline()
+		line = line.strip("\n")
+
+	girls_names = []
+
+	line = girl_file.readline()
+	line = line.strip("\n")
+	while line != "":
+		girls_names.append(line)
+		line = girl_file.readline()
+		line = line.strip("\n")
+
+	return boys_names, girls_names
+
+
+def find_name_in_file(user_name, boy_list, girl_list):
+	if user_name in boy_list:
+		print(user_name + " was found on the popular boys' names list.")
+	elif user_name in girl_list:
+		print(user_name + " was found on the popular girls' names list.")
+	else:
+		print(user_name + " was not found in the popular names lists.")
+
+
+main()
