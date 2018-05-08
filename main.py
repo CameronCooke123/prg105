0a172296154054c27b497681b1b2c4239de54e2a
CameronCooke123 <camo246810@yahoo.com>
2018-05-08 17:01:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 """
 
+	This is an application that allows you to save words that correspond to images.
+	When the application is run, you can add words & images, delete words & images, and search for existing words
+	to view their images. The words are case-sensitive.
 
 
+		WHERE I LEARNED HOW TO USE IMAGES:
 
-	# WHERE I LEARNED HOW TO IMPORT IMAGES:
+	There is no one place I where I learned how to import and display images.
+	Tkinter is able to work with images (although only a few formats), so with just a few google searches
+	it was pretty simple to figure out.
 
 """
 
 
 from tkinter import *
 from tkinter import filedialog
+import os
 
 
 class MainWindow:
@@ -19,14 +26,12 @@ class MainWindow:
 		self.search_button = Button(self.main_window, text="Search for word", command=self.search, width=20)
 		self.add_button = Button(self.main_window, text="Add new word", command=self.add_new, width=20)
 		self.delete_button = Button(self.main_window, text="Delete a word", command=self.delete, width=20)
-		self.save_button = Button(self.main_window, text="Save", command=self.save_lists, width=20)
 
 		self.exit_button = Button(self.main_window, text="Exit", command=quit, width=20)
 
 		self.search_button.pack()
 		self.add_button.pack()
 		self.delete_button.pack()
-		self.save_button.pack()
 		self.exit_button.pack()
 
 		self.initiate_lists()
@@ -54,15 +59,6 @@ class MainWindow:
 				line = line.strip("\n")
 
 			word_file.close()
-			print("words: ", word_list)
-
-			image_file = open("ImageSave.sav", "rb")
-			temp_list = image_file.read()
-
-			for image in temp_list:
-				image_list.append(image)
-
-			image_file.close()
 		except FileNotFoundError:
 			pass
 
@@ -73,13 +69,6 @@ class MainWindow:
 				word_file.write(word + "\n")
 
 		word_file.close()
-
-		image_file = open("ImageSave.sav", "w+b")
-		if len(image_list) > 0:
-			for image in image_list:
-				image_file.write(image)
-
-		image_file.close()
 
 
 class SearchWindow:
@@ -106,15 +95,14 @@ class SearchWindow:
 
 	def search_for_word(self):
 		for word in word_list:
-			print(word)
 			if word == self.entry_box.get():
 				i = word_list.index(word)
 				self.display_image(i)
 				return
-		not_found = ErrorWindow("Word not found")
-
+		not_found = MessageWindow("Word not found")
 
 	def display_image(self, index):
+		self.search_window.destroy()
 		display = DisplayWindow(index)
 
 
@@ -126,7 +114,6 @@ class DisplayWindow:
 		self.panel = Label(self.disp_window, image=self.image)
 
 		self.panel.pack()
-		print("Happened")
 		self.disp_window.mainloop()
 
 
@@ -156,16 +143,14 @@ class AddWindow:
 		word = self.entry_box.get()
 		word_list.append(word)
 		img = self.get_image()
-		image_list.append(img)
 
 		img_file = open(word + ".gif", "w+b")
 		img_file.write(img)
 		img_file.close()
 
-		print("Words: ", word_list)
-		print("Images: ", image_list)
-		# the add window needs to close
-		# the word needs to be saved
+		save_words()
+		self.add_window.destroy()
+		message = MessageWindow("Word successfully added")
 
 	def get_image(self):
 		file = filedialog.askopenfile(parent=self.add_window, mode="rb", title="Choose a file",
@@ -201,15 +186,16 @@ class DeleteWindow:
 	def delete_word(self):
 		try:
 			index = word_list.index(self.entry_box.get())
+			os.remove(word_list[index] + ".gif")
 			del word_list[index]
-			del image_list[index]
-			print("Words: ", word_list)
-			print("Images: ", image_list)
-		except ValueError:
-			not_found = ErrorWindow("Word not found")
+			save_words()
+			self.delete_window.destroy()
+			message = MessageWindow("Word successfully deleted")
+		except ValueError and FileNotFoundError:
+			not_found = MessageWindow("Word not found")
 
 
-class ErrorWindow:
+class MessageWindow:
 	def __init__(self, message):
 		self.window = Tk()
 
@@ -227,6 +213,15 @@ def main():
 
 
 word_list = []
-image_list = []
+
+
+def save_words():
+		word_file = open("WordSave.txt", "w+")
+		if len(word_list) > 0:
+			for word in word_list:
+				word_file.write(word + "\n")
+
+		word_file.close()
+
 
 main()
